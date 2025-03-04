cb5e2dc2a022ffc137c103f5e6652cc5e476e54b
Andris Buscariolli <buskarion@gmail.com>
2025-03-04 15:39:26 -0300

    refactor: remove buildDB from main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"github.com/buskarion/todoapp-with-gin/db"
 	"github.com/buskarion/todoapp-with-gin/router"
 )
 
 func main() {
-	db.BuildDB()
 	r := router.SetupRouter()
 	r.Run(":3000")
 }
